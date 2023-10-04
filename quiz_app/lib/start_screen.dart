aac527d5aad9557e8cdcfc7722d30ebd330d75eb
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-10-04 12:35:58 +0100

    Quiz App Opening Screen and learning continues

---
+++ quiz_app/lib/start_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          const SizedBox(
+            height: 30,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                fontFamily: 'Palatino'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              child: const Text('Start Quiz'))
+        ],
+      ),
+    );
+  }
+}
